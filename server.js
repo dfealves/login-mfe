61e9fa72faa37412530e41a4e728dfb51ea19ab9
dfealves <daniloferreira.alves@outlook.com>
2021-06-19 12:00:47 -0300

    add cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 //Importa as dependências que acabamos de instalar
 const express = require("express");
 const path = require("path");
+const cors = require("cors")
 
 const app = express();
 
+app.use(cors());
 // Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
 app.use(express.static(__dirname + "/dist/login-mfe"));
 
